51d990cee06b67745df982331cb723bebc4c5133
phuongnguyen <phuongnguyen240805@gmail.com>
2025-07-20 10:20:35 +0700

    create mongoDB atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // connect db
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/phuong_note_app_dev');
+        await mongoose.connect(`${process.env.MONGODB_URI}/phuong_note_app-db`);
         console.log('connect successfully!!!');
     } catch (error) {
         throw new Error(error);
